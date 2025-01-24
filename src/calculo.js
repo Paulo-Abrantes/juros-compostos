cfe4f151cd32cac9927b1a3e770593ea77912574
Paulo-Abrantes <pauloaugusto123123@gmail.com>
2025-01-24 14:18:07 -0300

    Inicializa o repositório com README e estrutura de pastas

---
+++ src/calculo.js
@@ -0,0 +1,12 @@
+function calcularJurosCompostos(capital, taxa, tempo) {
+    return capital * Math.pow((1 + taxa / 100), tempo);
+}
+
+// Exemplo de uso
+const capitalInicial = 1000;
+const taxaJuros = 5; // 5% ao mês
+const tempoMeses = 12;
+
+console.log(`Montante final: R$ ${calcularJurosCompostos(capitalInicial, taxaJuros, tempoMeses).toFixed(2)}`);
+
+module.exports = calcularJurosCompostos;
