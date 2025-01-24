cdbca375fa4463d1e629532d188c6e3d3e466d01
Paulo-Abrantes <pauloaugusto123123@gmail.com>
2025-01-24 14:25:30 -0300

    Adiciona função para calcular juros compostos

--- src/calculo.js
+++ src/calculo.js
@@ -10,3 +10,9 @@ const tempoMeses = 12;
 console.log(`Montante final: R$ ${calcularJurosCompostos(capitalInicial, taxaJuros, tempoMeses).toFixed(2)}`);
 
 module.exports = calcularJurosCompostos;
+// src/calculo.js
+
+// src/calculo.js
+function calcularJurosCompostos(principal, taxa, tempo) {
+    return principal * Math.pow((1 + taxa / 100), tempo);
+}  
